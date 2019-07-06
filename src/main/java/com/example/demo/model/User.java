b35402bae27ae7c32dd2e8d45acb4c8dbd62433e
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-07-06 17:18:50 +0800

    Add model for users and user_profile

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,99 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+    @NotNull
+    @Email
+    @Size(max = 100)
+    @Column(unique = true)
+    private String email;
+    
+    @NotNull
+    @Size(max = 65)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Size(max = 65)
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotNull
+    @Size(max = 128)
+    private String password;
+    
+    @OneToOne(fetch = FetchType.LAZY,
+            cascade =  CascadeType.ALL,
+            mappedBy = "user")
+    private UserProfile userProfile;
+
+    // Hibernate requires a no-arg constructor
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+}
